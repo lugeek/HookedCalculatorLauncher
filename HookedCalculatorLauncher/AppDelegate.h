ae76aaee7a644a7637e540dcded3b4d8826f4d04
lugeek <me@lugeek.com>
2020-09-24 19:33:50 +0800

    Initial Commit

---
+++ HookedCalculatorLauncher/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  HookedCalculatorLauncher
+//
+//  Created by lugeek on 2020/9/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ HookedCalculatorLauncher/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  HookedCalculatorLauncher
+//
+//  Created by lugeek on 2020/9/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
